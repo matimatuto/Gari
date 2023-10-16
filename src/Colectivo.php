a309e886111c3deb76381d16056266aade2947d0
matias gari <matimatuto@gmail.com>
2023-10-16 20:30:49 -0300

    iteracion3 test12

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -12,45 +12,47 @@ class Colectivo {
         $this->mitadTarifa = $this->tarifa/2;
     }
 
-    public function pagarCon($tarjeta, $boleto, $tiempo = 0) {
+    public function pagarCon($tarjeta,$boleto,$tiempo=0){
         $boletoDevuelto = false;
-    
-        switch (get_class($tarjeta)) {
+        switch(get_class($tarjeta)) {
             case 'TrabajoSube\TarjetaFranquiciaCompleta':
-                if ($tarjeta->habilitada) {
+                if($tarjeta->habilitada) {
                     $tarjeta->descargarSaldo($this->sinTarifa);
                     $tarjeta->registrarViaje($tiempo);
-                    $boleto->actualizarBoleto($this->linea, $this->sinTarifa, $tarjeta->saldo, $tarjeta->tipo, $tarjeta->ID);
-    
+                    $boleto->actualizarBoleto($this->linea,$this->sinTarifa,$tarjeta->saldo,$tarjeta->tipo,$tarjeta->ID);
+
                     $boletoDevuelto = true;
-                    break;
+                    return $boleto;
                 }
             case 'TrabajoSube\TarjetaFranquiciaParcial':
-                if (!$boletoDevuelto && $tarjeta->verificarHabilitada($tiempo)) {
-                    if ($tarjeta->saldo > $this->mitadTarifa) {
-                        $tarjeta->descargarSaldo($this->mitadTarifa);
-                        $tarjeta->registrarViaje($tiempo);
-                        $boleto->actualizarBoleto($this->linea, $this->mitadTarifa, $tarjeta->saldo, $tarjeta->tipo, $tarjeta->ID);
-    
-                        $boletoDevuelto = true;
-                        break;
+                if(!$boletoDevuelto) {
+                    if($tarjeta->verificarHabilitada($tiempo)) {
+                        if($tarjeta->saldo >= $this->mitadTarifa) {
+                            $tarjeta->descargarSaldo($this->mitadTarifa);
+                            $tarjeta->registrarViaje($tiempo);
+                            $boleto->actualizarBoleto($this->linea,$this->mitadTarifa,$tarjeta->saldo,$tarjeta->tipo,$tarjeta->ID);
+
+                            $boletoDevuelto = true;
+                            return $boleto;
+                        }
                     }
                 }
             default:
-                if (!$boletoDevuelto) {
-                    if ($tarjeta->saldo > $this->tarifa) {
+                if(!$boletoDevuelto) {
+                    if($tarjeta->saldo > $this->tarifa) {
                         $tarjeta->descargarSaldo($this->tarifa);
-                        $boleto->actualizarBoleto($this->linea, $this->tarifa, $tarjeta->saldo, $tarjeta->tipo, $tarjeta->ID);
-    
-                        return $boleto;
-                    } elseif ($tarjeta->saldo - $this->tarifa > -211.84 && $tarjeta->plus > 0) {
-                        $tarjeta->descargarSaldo($this->tarifa);
-                        $boleto->actualizarBoleto($this->linea, $this->tarifa, $tarjeta->saldo, $tarjeta->tipo, $tarjeta->ID);
-                        $tarjeta->plus--;
-    
+                        $boleto->actualizarBoleto($this->linea,$this->tarifa,$tarjeta->saldo,$tarjeta->tipo,$tarjeta->ID);
+
                         return $boleto;
                     }
-    
+                    elseif($tarjeta->saldo - $this->tarifa > -211.84 && $tarjeta->plus > 0) {
+                        $tarjeta->descargarSaldo($this->tarifa);
+                        $boleto->actualizarBoleto($this->linea,$this->tarifa,$tarjeta->saldo,$tarjeta->tipo,$tarjeta->ID);
+                        $tarjeta->plus --;
+
+                        return $boleto;
+                    }
+
                     return false;
                 }
         }
