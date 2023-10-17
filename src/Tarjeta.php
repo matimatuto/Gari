ac766685f24a8ded0812d2786c08155347b35f80
matias gari <matimatuto@gmail.com>
2023-10-16 22:51:53 -0300

    iteracion3 test32

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -16,25 +16,28 @@ class Tarjeta {
     }
 
     public function cargarTarjeta($cargarSaldo) {
-        if (in_array($cargarSaldo,[150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000])) {
-            if($this->saldo + $cargarSaldo < 6600){
-                $this->saldo += $cargarSaldo;
-                if ($this->saldo > 0) {
-                    $this->plus = 2;
-                }
-            
-                return true;
+    if (in_array($cargarSaldo,[150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000])) {
+        if($this->saldo + $cargarSaldo <= 6600){
+            $this->saldo += $cargarSaldo;
+            if ($this->saldo > 0) {
+                $this->plus = 2;
             }
-            else {
-                $this->saldoExtra = $cargarSaldo - $this->saldo;
-
-                return true;
-            }
+        
+            return true;
         }
         else {
-            return false;
+            $excess = $this->saldo + $cargarSaldo - 6600;
+            $this->saldo = 6600;
+            $this->saldoExtra = $excess;
+
+            return true;
         }
     }
+    else {
+        return false;
+    }
+}
+
 
     public function descargarSaldo($restarSaldo) {
         $this->historialSaldo[] = $this->saldo;
